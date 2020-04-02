1e0d3d8adf864b0a52c3e216b7a03266a31b6601
Israel <Israel@ISRAEL1660-DESK>
2020-04-01 23:56:32 -0500

    Almost

--- UniversityRegistry/Data/Person.cs
+++ UniversityRegistry/Data/Person.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace UniversityRegistry.Data
 {
-    /// <summary>
+        /// <summary>
     /// A class representing a person associated with the university
     /// </summary>
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
+    /// <summary>
+    /// Event triggered when properties of Person change
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// The next ID to assign to a newly-created person
         /// </summary>
@@ -19,10 +25,20 @@ namespace UniversityRegistry.Data
         /// </summary>
         public uint ID { get; private set; }
 
+        private string firstName;
         /// <summary>
         /// The person's first name
         /// </summary>
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName == value) return;
+                firstName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FirstName"));
+            }
+        }
 
         /// <summary>
         /// The person's last name
@@ -50,6 +66,35 @@ namespace UniversityRegistry.Data
         public Person()
         {
             ID = NextID++;
+        }
+
+        /// <summary>
+        /// Returns a string identifying the person
+        /// </summary>
+        /// <returns>A string consisting of last name, first name, and ID</returns>
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} [{ID}]";
+        }
+
+        private Role role;
+        /// <summary>
+        /// The person's role
+        /// </summary>
+        public Role Role
+        {
+            get => role;
+            set
+            {
+                if (role == value) return;
+                role = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsUndergraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsGraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsFaculty"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsStaff"));
+            }
         }
     }
 }
